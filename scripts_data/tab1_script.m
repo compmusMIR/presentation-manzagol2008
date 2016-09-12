443a80687ea5597d42abc153533cf373f415e47c
tiko <tiko@ime.usp.br>
2016-09-12 17:01:17 -0300

    melhor tab1 imagem

--- scripts_data/tab1_script.m
+++ scripts_data/tab1_script.m
@@ -1,3 +1,12 @@
+set term png size 1024,768
+set output "../src/img/tab1.png"
+
+set terminal png enhanced font 'Verdana,18'
+
+#set key font ",18"
+#set xtics font "Verdana,18"
+#set xtics font "Verdana,18"
+
 set xtics ("10³" 30, "10⁴" 130, "2.5*10⁴" 230, "5*10⁴" 330,)
 
 set key left top
@@ -8,8 +17,8 @@ set xrange [-30:400]
 set xlabel ('Max number of spikes allowed')
 set ylabel ('Signal to Noise Ratio (SNR) [dB]')
 
+
 plot 	'tab1.dat' every ::0 using 1:2 with boxes ls 1 t '32 kernels', \
 		'tab1.dat' every ::1 using 1:2 with boxes ls 2 t '64 kernels', \
 		'tab1.dat' every ::2 using 1:2 with boxes ls 3 t '128 kernels'
 
-pause -1
