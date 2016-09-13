a7a305dd41aeb789742ed917582e002a8269c863
tiko <tiko@ime.usp.br>
2016-09-13 09:43:15 -0300

    meu conteúdo pronto

---
+++ scripts_data/tab4_script.m
@@ -0,0 +1,21 @@
+set term png size 1024,768
+set output "../src/img/tab4.png"
+set terminal png enhanced font 'Verdana,18'
+
+datfile="tab4.dat"
+
+set xtics ("10³" 15, "10⁴" 115,)
+
+set key left top
+set boxwidth 30
+set style fill solid
+set yrange [0:100]
+set xrange [-30:160]
+set xlabel ('Max number of spikes allowed')
+set ylabel ('Signal to Noise Ratio (SNR) [dB]')
+
+
+plot 	datfile every ::0 using 1:2 with boxes ls 1 t 'gammatone kernels', \
+		datfile every ::1 using 1:2 with boxes ls 2 t 'learned kernels'
+
+#pause -1
